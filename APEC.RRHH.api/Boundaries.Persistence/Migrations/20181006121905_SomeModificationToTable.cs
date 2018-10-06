9caf6df9a7b2c7e3fe89760c121483a2e023cac6
Ezequiel Rodriguez <ezrod12@gmail.com>
2018-10-06 14:43:38 -0400

    Added a migrations

---
+++ APEC.RRHH.api/Boundaries.Persistence/Migrations/20181006121905_SomeModificationToTable.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Boundaries.Persistence.Migrations
+{
+    public partial class SomeModificationToTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Users");
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuantityOfEmployeesNeeded",
+                table: "Jobs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Jobs",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Employees",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuantityOfEmployeesNeeded",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Jobs");
+
+            migrationBuilder.DropColumn(
+                name: "Status",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<int>(
+                name: "Status",
+                table: "Users",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
